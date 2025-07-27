52b8d1314e8c4131c7d0c43c8dee2252c79a65d8
hamidkrml <karimlihamid4@gmail.com>
2025-07-27 23:52:47 +0400

    ✨ feat(init): Uygulama başlatma sürecini güncelle ve hata yönetimi ekle; bağımlılıkları güncelle

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:architecture_template/feature/home/view/mixin/home_view.dart';
+import 'package:architecture_template/product/init/application_initialize.dart';
 import 'package:architecture_template/product/init/product_locazalition.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
+  await ApplicationInitialize().make();
   runApp(ProductLocazalition(child: const MyApp()));
 }
 
--- lib/product/init/application_initialize.dart
+++ lib/product/init/application_initialize.dart
@@ -1,17 +1,41 @@
+import 'dart:async';
+
 import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_logger/easy_logger.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kartal/kartal.dart';
+import 'package:logger/web.dart';
 
 /// This class is used to initialize the application.
 @immutable
 final class ApplicationInitialize {
   /// it's only use for setup business
-  const ApplicationInitialize.setup();
+  // const ApplicationInitialize.setup();
+
+  Future<void> make() async {
+    await runZonedGuarded(
+      () async => await _initialize(),
+      (error, stack) {
+        Logger().e(error);
+      },
+    );
+  }
 
   /// this method is used to initialize the application process
 
-  Future<void> main() async {
+  Future<void> _initialize() async {
     WidgetsFlutterBinding.ensureInitialized();
     await EasyLocalization.ensureInitialized();
     EasyLocalization.logger.enableBuildModes = [];
+    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    await DeviceUtility.instance.initPackageInfo();
+    FlutterError.onError = (details) {
+      /// crashlytics log insert here
+      ///
+      /// custom service or custom logger insert here
+      ///
+      Logger().e(details.exceptionAsString());
+    };
   }
 }
